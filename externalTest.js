73d5e3e8be8a125a5e7e8be8c6c295c2ae45902f
Heckbringer <erik.volkert@viasat.com>
2016-06-13 14:48:25 -0400

    Create externalTest.js

---
+++ externalTest.js
@@ -0,0 +1,126 @@
+function sleep(milliseconds){
+  var start = new Date().getTime();
+  var keep_looping = true;
+  while (keep_looping) {
+    if ((new Date().getTime() - start)> milliseconds) {
+      keep_looping = false;
+    }
+  }
+}
+
+sleep(2000);
+if (https == "yes") {
+  document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=1rnd=" + Math.random() + "'>");
+}
+else if (https == "no") {
+  document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=1rnd=" + Math.random() + "'>");
+}
+
+sleep(2000);
+for (i = 0; i < numberOfImages; i++) {
+  if (https == "yes") {
+    document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=1&rnd=" + Math.random() + "'>");
+  }
+  else if (https == "no") {
+    document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=1&rnd=" + Math.random() + "'>");
+  }
+}
+
+sleep(2000);
+
+for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=2&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=2&rnd=" + Math.random() + "'>");
+    }
+  }
+
+  sleep(2000);
+
+  for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=3&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=3&rnd=" + Math.random() + "'>");
+    }
+  }
+
+  sleep(2000);
+
+  for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=4&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=4&rnd=" + Math.random() + "'>");
+    }
+  }
+
+  sleep(2000);
+
+  for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=5&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src=http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=5&rnd=" + Math.random() + "'>");
+    }
+  }
+
+  sleep(2000);
+
+  for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=6&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=6&rnd=" + Math.random() + "'>");
+    }
+  }
+
+  sleep(2000);
+
+  for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=7&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=7&rnd=" + Math.random() + "'>");
+    }
+  }
+
+  sleep(2000);
+
+  for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=8&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=8&rnd=" + Math.random() + "'>");
+    }
+  }
+
+  sleep(2000);
+
+  for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=9&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=9&rnd=" + Math.random() + "'>");
+    }
+  }
+
+  sleep(2000);
+
+  for (i = 0; i < numberOfImages; i++) {
+    if (https == "yes") {
+      document.write("<img src='https://s.yimg.com/dh/ap/default/130909/y_200_a.png?req=10&rnd=" + Math.random() + "'>");
+    }
+    else if (https == "no") {
+      document.write("<img src='http://l.yimg.com/av/curveball/ads/pr/RESIZE/82x82/643a0ed39be1c49502e9ac5a0b519dc7.jpeg?req=10&rnd=" + Math.random() + "'>");
+    }
+  }
